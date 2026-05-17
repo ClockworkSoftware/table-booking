93d24160ddf301a1c150e6fb0b4a74fe94dc8d71
DragonMadness <supervasya45@gmail.com>
2026-05-17 14:41:30 +0300

    fixed gradle configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
 
 group = "org.clockwork.table-booking"
 version = "1.0.0"
-
-//java {
-//    toolchain {
-//        languageVersion = JavaLanguageVersion.of(21)
-//    }
-//}
